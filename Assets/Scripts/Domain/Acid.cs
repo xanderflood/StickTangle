13004c512689bfb72347f53edb38e9b4353d3b75
Gulshan Singh <gulshan@umich.edu>
2014-03-31 15:48:52 -0400

    Removed code that isn't needed

--- Assets/Scripts/Domain/Acid.cs
+++ Assets/Scripts/Domain/Acid.cs
@@ -6,10 +6,6 @@ using SquareType = Grid.SquareType;
 
 public class Acid : MonoBehaviour {
     private void Start() {
-        Grid grid = Utils.FindComponent<Grid>("Board");
-        Position pos = grid.CoordToPos(transform.position, false);
-        if (grid.InBounds(pos)) { // TODO
-            grid.acidBlocks.Add(this);
-        }
+    	grid.acidBlocks.Add(this);
     }
 }
