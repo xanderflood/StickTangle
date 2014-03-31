7a768d562d9dbb4be0ebceb65c843f146edc4935
Gulshan Singh <gulshan@umich.edu>
2014-03-31 10:44:27 -0400

    Added TeleportPair to group Teleporters together

---
+++ Assets/Scripts/TeleportPair.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportPair : MonoBehaviour {
+	private void Start() {
+		Utils.Assert(transform.childCount == 2);
+		Teleporter first = Utils.GetComponent<Teleporter>(transform.GetChild(0).gameObject);
+		Teleporter second = Utils.GetComponent<Teleporter>(transform.GetChild(1).gameObject);
+
+		first.rowOther = second.row;
+		first.colOther = second.col;
+
+		second.rowOther = first.row;
+		second.colOther = first.col;
+	}
+}
