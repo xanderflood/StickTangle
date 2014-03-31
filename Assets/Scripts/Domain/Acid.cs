54cb23dcb9c77ad373b2b268761e0709b4c90d96
go-blue-matthew <mcornett94@gmail.com>
2014-03-31 16:07:23 -0400

    added find component

--- Assets/Scripts/Domain/Acid.cs
+++ Assets/Scripts/Domain/Acid.cs
@@ -6,6 +6,7 @@ using SquareType = Grid.SquareType;
 
 public class Acid : MonoBehaviour {
     private void Start() {
+		Grid grid = Utils.FindComponent<Grid>("Board");
     	grid.acidBlocks.Add(this);
     }
 }
