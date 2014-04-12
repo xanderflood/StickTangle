1272939917009ef2952a090ce6dabf6433d465f1
Gulshan Singh <gulshan@umich.edu>
2014-04-12 19:54:37 -0400

    Fixed MusicSelector script

--- Assets/MusicSelector.cs
+++ Assets/MusicSelector.cs
@@ -8,29 +8,27 @@ public class MusicSelector : MonoBehaviour {
 	public AudioClip melody3;
 	public AudioClip melody4;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	private void Update () {
+		string name = Application.loadedLevelName;
 
-		if ((Application.loadedLevelName == "Level1.1")||(Application.loadedLevelName == "Level1.2")||
-		    (Application.loadedLevelName == "Level1.3")||(Application.loadedLevelName == "Level1.4")||
-		    (Application.loadedLevelName == "Level1.5")||(Application.loadedLevelName == "Level1.6")||
-		    (Application.loadedLevelName == "Level1.7")||(Application.loadedLevelName == "Level1.8")){
-
+		if (name == "Level1.1" || name == "Level1.2" || name == "Level1.3" || name == "Level1.4" ||
+		    name == "Level1.5" || name == "Level1.6" || name == "Level1.7" || name == "Level1.8") {
 			audio.clip = melody1;
 			if (!audio.isPlaying)
 				audio.Play(); 
 		}
+<<<<<<< HEAD
 		if ((Application.loadedLevelName == "Level2.1")){
+=======
+
+		if (name == "Level2.1"){
+>>>>>>> Fixed MusicSelector script
 			if (!audio.isPlaying)
 				audio.clip = melody2;
 			if (!audio.isPlaying)
 				audio.Play(); 
 		}
+<<<<<<< HEAD
 		if ((Application.loadedLevelName == "Level2.2")||(Application.loadedLevelName == "Level2.3")||
 		    (Application.loadedLevelName == "Level2.4")||(Application.loadedLevelName == "Level2.5")){
 			audio.clip = melody2;
@@ -38,27 +36,49 @@ public class MusicSelector : MonoBehaviour {
 				audio.Play(); 
 		}	
 		if ((Application.loadedLevelName == "Level3.1")){
+=======
+
+		if (name == "Level2.2" || name == "Level2.3" || name == "Level2.4" || name == "Level2.5"){
+			audio.clip = melody2;
+			if (!audio.isPlaying)
+				audio.Play(); 
+		}
+		
+		if (name == "Level3.1"){
+>>>>>>> Fixed MusicSelector script
 			if (!audio.isPlaying)
 				audio.clip = melody3;
 			if (!audio.isPlaying)
 				audio.Play(); 
 		}
+<<<<<<< HEAD
 		if ((Application.loadedLevelName == "Level3.2")||(Application.loadedLevelName == "Level3.3")){
+=======
+
+		if (name == "Level3.2" || name == "Level3.3"){
+>>>>>>> Fixed MusicSelector script
 			audio.clip = melody3;
 			if (!audio.isPlaying)
 				audio.Play(); 
 		}
+<<<<<<< HEAD
 		if ((Application.loadedLevelName == "Level4.1")){
+=======
+
+		if (name == "Level4.1"){
+>>>>>>> Fixed MusicSelector script
 			if (!audio.isPlaying)
 				audio.clip = melody4;
 			if (!audio.isPlaying)
 				audio.Play(); 
 		}
+<<<<<<< HEAD
 		if ((Application.loadedLevelName == "Level4.2")){
 			audio.clip = melody4;
 			if (!audio.isPlaying)
 				audio.Play(); 
 		}
+=======
+>>>>>>> Fixed MusicSelector script
 	}
-
 }
