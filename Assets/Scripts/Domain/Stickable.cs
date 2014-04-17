b65e212afe0aa5bfb61430dccea9ec920f8e3238
Gulshan Singh <gulshan@umich.edu>
2014-04-17 15:47:09 -0400

    Play acid sound from Piece

--- Assets/Scripts/Domain/Stickable.cs
+++ Assets/Scripts/Domain/Stickable.cs
@@ -5,11 +5,8 @@ using Square = Grid.Square;
 using SquareType = Grid.SquareType;
 
 public class Stickable : Piece {
-
-	private MusicSelector music;
-	
+		
 	private void Start() {
-		music = Utils.FindComponent<MusicSelector>("Music");
 		grid.SetSquare(row, col, new Square(SquareType.Stickable));
 		
 		Sticker s = Utils.FindComponent<Sticker>("Player");
@@ -17,9 +14,8 @@ public class Stickable : Piece {
 	}
 
 	public override void DestroyPiece() {
-		music.playAcid ();
 		gameObject.renderer.enabled = false;
-		StartCoroutine (AdvanceAcid ());
+		StartCoroutine(AdvanceAcid());
 	}
 
 	private IEnumerator AdvanceAcid() {
