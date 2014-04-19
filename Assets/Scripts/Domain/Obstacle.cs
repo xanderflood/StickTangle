1b1371f243b13733b07b649ee11f7cfa536bb559
alexcope <alexcope@umich.edu>
2014-04-19 19:26:30 -0400

    More graphics updates

--- Assets/Scripts/Domain/Obstacle.cs
+++ Assets/Scripts/Domain/Obstacle.cs
@@ -1,10 +1,28 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 using Square = Grid.Square;
 using SquareType = Grid.SquareType;
 
 public class Obstacle : MonoBehaviour {
+
+    public List<Material> CrayonMats;
+
+    void Awake()
+    {
+
+        this.renderer.material = CrayonMats[Random.Range(0, CrayonMats.Count)];
+        this.transform.Rotate(0, 0, Random.Range(0, 3) * 90);
+        Color temp = new Color();
+        temp.r = .1f;
+        temp.g = .2f;
+        temp.b = .8f;
+        temp.a = 1;
+        this.renderer.material.color = temp;
+
+    }
+
 	private void Start() {
 		Grid grid = Utils.FindComponent<Grid>("Board");
 		Position pos = grid.CoordToPos(transform.position, false);
