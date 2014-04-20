842676fa904e0372a2bc8256a614460b56ba339c
alexcope <alexcope@umich.edu>
2014-04-19 21:23:36 -0400

    teleporters have texture
    and dont work

---
+++ Assets/Scripts/Domain/TeleporterBlock.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TeleporterBlock : Piece {
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        Color temp = new Color();
+        temp.r = 230f/255;
+        temp.g = 230f/255;
+        temp.b = 0f;
+        temp.a = 1;
+        this.renderer.material.color = temp;
+        Debug.Log("WHATHTEFUCK");
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
