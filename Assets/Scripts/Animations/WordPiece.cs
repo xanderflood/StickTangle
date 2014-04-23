581301e28d68390d2689d86471687cc68f744f15
Xander Flood <xander.t.flood@gmail.com>
2014-04-22 22:47:41 -0400

    Some prefabs necessary for the main menu animation

---
+++ Assets/Scripts/Animations/WordPiece.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WordPiece : MonoBehaviour {
+
+	public GameObject letterModel;
+	public string word;
+
+	LetterPiece[] gos;
+
+	// Use this for initialization
+	void Start () {
+
+		gos = new LetterPiece[word.Length];
+		int i = 0;
+		Vector3 position = transform.position;
+		foreach (char l in word) {
+
+			gos[i] = (((GameObject)Instantiate(letterModel, position, Quaternion.identity))
+			          .transform.FindChild("Block").GetComponent<LetterPiece>());
+			gos[i].letter = l.ToString();
+			
+			gos[i].BGcolor = RandomColor(0.3f);
+			gos[i].fontColor = RandomColor(1f);
+
+			gos[i].gameObject.transform.parent.parent = gameObject.transform;
+
+			position.x += 2f;
+			++i;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static Color RandomColor(float a = 1f) {
+		Color c;
+		c.r = Random.Range(0f, 1f);
+		c.b = Random.Range(0f, 1f);
+		c.g = Random.Range(0f, 1f);
+		c.a = a;
+		return c;
+	}
+}
