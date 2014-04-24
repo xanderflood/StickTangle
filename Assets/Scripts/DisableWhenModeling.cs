f58af72c4b7b3a26fcacd02e73ab978ee3d83d7e
Xander Flood <xander.t.flood@gmail.com>
2014-04-24 00:06:42 -0400

    no restart text in preview

---
+++ Assets/Scripts/DisableWhenModeling.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableWhenModeling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (LevelManager.modeling)
+			GameObject.Destroy(gameObject);
+	}
+}
