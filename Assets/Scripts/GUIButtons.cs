e069388869e7787402e0dd58203ab302db36f5a3
alexcope <alexcope@umich.edu>
2014-04-23 22:57:43 -0400

    fixed menu buttons moving with resolution
    they might be kind of small now

--- Assets/Scripts/GUIButtons.cs
+++ Assets/Scripts/GUIButtons.cs
@@ -5,6 +5,8 @@ public class GUIButtons : MonoBehaviour {
 
 	private LevelManager lm;
 
+    float virtualWidth = 960.0f; //create gui for this size, use matrix to automaticly scale it
+    float virtualHeight = 600.0f;
 	private void Start() {
 		lm = Utils.FindComponent<LevelManager>("LevelManager");
 	}
@@ -12,12 +14,13 @@ public class GUIButtons : MonoBehaviour {
 	private void OnGUI() {
 		if (LevelManager.modeling)
 			return;
+        GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(Screen.width / virtualWidth, Screen.height / virtualHeight, 1.0f));
 
-		if ((GUI.Button(new Rect(650, 30, 50, 20), "Menu")) || (Input.GetKey(KeyCode.Joystick1Button18))){
+ 		if ((GUI.Button(new Rect(732, 40, 45, 20), "Menu")) || (Input.GetKey(KeyCode.Joystick1Button18))){
 			Application.LoadLevel("LevelSelect");
 		}
 		
-		if ((GUI.Button(new Rect(720, 30, 50, 20), "Skip")) || (Input.GetKey(KeyCode.Joystick1Button17))) {
+		if ((GUI.Button(new Rect(825, 40, 45, 20), "Skip")) || (Input.GetKey(KeyCode.Joystick1Button17))) {
 			lm.AdvanceLevel();
 		}
 	}
