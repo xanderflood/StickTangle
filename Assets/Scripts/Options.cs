08538f2e9c7e38ab6ef82f08fa4a9782847b5390
Gulshan Singh <gulshan@umich.edu>
2014-04-24 11:27:10 -0400

    Added an options scene

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Options : MonoBehaviour {
+
+	public GUIStyle textStyle;
+	private GUIStyle toggleStyle;
+
+	private bool colorblindMode = false;
+	private float volume = 100;
+
+	private void OnGUI() {
+		if (toggleStyle == null) {
+			toggleStyle = new GUIStyle(GUI.skin.toggle);
+			toggleStyle.normal.textColor = Color.black;
+			toggleStyle.hover.textColor = Color.grey;
+			toggleStyle.active.textColor = Color.grey;
+			toggleStyle.onNormal.textColor = Color.black;
+			toggleStyle.onHover.textColor = Color.grey;
+			toggleStyle.onActive.textColor = Color.grey;
+		}
+
+		colorblindMode = GUI.Toggle(new Rect(30, 30, 170, 20), colorblindMode, "Enable colorblind mode", toggleStyle);
+		volume = GUI.HorizontalSlider(new Rect(30, 60, 100, 20), volume, 0, 100);
+		GUI.Label(new Rect(140, 60, 50, 20), "Volume", textStyle);
+		
+	}
+}
